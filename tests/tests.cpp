09e4b277261b15ebd03f9200a946224a59afa776
Andrey22250 <andrchepras@mail.ru>
2026-03-17 00:37:29 +0700

    Add test for ExpressionEngine class instancing

---
+++ tests/tests.cpp
@@ -0,0 +1,6 @@
+#include "../include/catch_amalgamated.hpp"
+#include "../src/ExpressionEngine.cpp"
+
+TEST_CASE("Проверка существования класса ExpressionEngine", "[ExpressionEngine]") {
+	ExpressionEngine engine;
+}
