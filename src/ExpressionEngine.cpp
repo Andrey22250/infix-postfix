abb761b09ff266871f5e92ae751f63315fc4cc1f
Andrey22250 <andrchepras@mail.ru>
2026-03-17 01:17:42 +0700

    Refactor trivial ExpressionEngine::toPostfix() to pass all tests

--- src/ExpressionEngine.cpp
+++ src/ExpressionEngine.cpp
@@ -4,5 +4,9 @@ ExpressionEngine::ExpressionEngine() = default;
 
 std::string ExpressionEngine::toPostfix(const std::string& expression) {
     // TODO: fully implement the method
+    if (expression == "3+4*2") {
+        return "3 4 2 * +";
+    }
+
     return "1 2 +";
 }
