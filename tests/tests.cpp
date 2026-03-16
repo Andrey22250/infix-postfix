10900f0b143db275ec4301735f46d6d0ffbf7413
Andrey22250 <andrchepras@mail.ru>
2026-03-17 01:49:08 +0700

    Add more tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -24,3 +24,34 @@ TEST_CASE("Вычисление сложного выражения в ОПЗ", 
     ExpressionEngine engine;
     REQUIRE_THAT(engine.evaluate("3 5 2 / -"), Catch::Matchers::WithinRel(0.5, 0.0001));
 }
+
+TEST_CASE("Точность вычисления вещественных чисел", "[ExpressionEngine]") {
+    ExpressionEngine engine;
+
+    SECTION("Деление с остатком") {
+        REQUIRE_THAT(engine.evaluate("5 2 /"), Catch::Matchers::WithinRel(2.5, 0.0001));
+    }
+
+    SECTION("Сложное выражение в ОПЗ") {
+        // (3 + 4 * 2) / (1 - 5) = 11 / -4 = -2.75
+        REQUIRE_THAT(engine.evaluate("3 4 2 * + 1 5 - /"), Catch::Matchers::WithinRel(-2.75, 0.0001));
+    }
+}
+
+TEST_CASE("Обработка ошибок и исключений", "[ExpressionEngine]") {
+    ExpressionEngine engine;
+
+    SECTION("Деление на ноль") {
+        REQUIRE_THROWS_AS(engine.evaluate("10 0 /"), std::runtime_error);
+    }
+
+    SECTION("Некорректные скобки") {
+        REQUIRE_THROWS_AS(engine.toPostfix("(1+2"), std::invalid_argument);
+        REQUIRE_THROWS_AS(engine.toPostfix("1+2)"), std::invalid_argument);
+    }
+
+    SECTION("Некорректное входное выражение") {
+        REQUIRE_THROWS_AS(engine.evaluate("1 2 + +"), std::runtime_error);
+        REQUIRE_THROWS_AS(engine.evaluate("1 2 3 +"), std::runtime_error);
+    }
+}
