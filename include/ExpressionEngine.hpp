e2441c44be5b4d82ce12d74f151bcc540f04f9b1
Andrey22250 <andrchepras@mail.ru>
2026-03-17 01:13:30 +0700

    Add trivial ExpressionEngine::toPostfix() implementation

--- include/ExpressionEngine.hpp
+++ include/ExpressionEngine.hpp
@@ -1,10 +1,12 @@
 #ifndef EXPRESSION_ENGINE_HPP
 #define EXPRESSION_ENGINE_HPP
 
+#include <string>
+
 class ExpressionEngine {
 public:
-
     ExpressionEngine();
+    std::string toPostfix(const std::string& expression);
 };
 
 #endif // EXPRESSION_ENGINE_HPP
--- src/ExpressionEngine.cpp
+++ src/ExpressionEngine.cpp
@@ -1,3 +1,8 @@
 #include "../include/ExpressionEngine.hpp"
 
 ExpressionEngine::ExpressionEngine() = default;
+
+std::string ExpressionEngine::toPostfix(const std::string& expression) {
+    // TODO: fully implement the method
+    return "1 2 +";
+}
