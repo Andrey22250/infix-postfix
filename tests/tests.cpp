14ee948cebdca5411c45ebf88348d0be13865146
Andrey22250 <andrchepras@mail.ru>
2026-03-17 01:05:13 +0700

    Add test for simple expression in RPN

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,3 +4,8 @@
 TEST_CASE("Проверка существования класса ExpressionEngine", "[ExpressionEngine]") {
 	ExpressionEngine engine;
 }
+
+TEST_CASE("Преобразование простого выражения в ОПЗ", "[ExpressionEngine]") {
+    ExpressionEngine engine;
+    REQUIRE(engine.toPostfix("1+2") == "1 2 +");
+}
